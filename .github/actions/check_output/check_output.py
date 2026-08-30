d89c2ad6159faffe35c1e9f34791603cf99fbda8
Ahmed <ahmed.akod@outlook.com>
2026-08-30 19:45:29 -0400

    test: change test to use set for flexible testing

--- .github/actions/check_output/check_output.py
+++ .github/actions/check_output/check_output.py
@@ -6,5 +6,5 @@ expected_output_files = json.loads(environ["EXPECTED_OUTPUT_FILES"])
 actual_input_files = json.loads(environ["ACTUAL_INPUT_FILES"])
 actual_output_files = json.loads(environ["ACTUAL_OUTPUT_FILES"])
 
-assert expected_input_files == actual_input_files
-assert expected_output_files == actual_output_files
+assert set(expected_input_files) == set(actual_input_files)
+assert set(expected_output_files) == set(actual_output_files)
